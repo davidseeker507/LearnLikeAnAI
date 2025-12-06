fced250e1ecd3efc31066ce7690772d37849fe27
David Li <david@Big-Ds-Macbook.local>
2025-12-06 17:44:53 -0600

    complete supervised page 2

--- styles/main.js
+++ styles/main.js
@@ -1,15 +1,48 @@
 const userGuess = document.getElementById('userGuess');
+const submitButton = document.getElementById('submitButton');
+const questionElement = document.getElementById('question'); 
+const outputElement = document.getElementById('output');
 
+const questions = [
+    { question: 'What is 5-2?', answer: 3 },
+    { question: 'What is 5+7?', answer: 12 },
+    { question: 'What is 3+1?', answer: 4 },
+    { question: 'What is 10-5?', answer: 5 }
+];
+
+let currentQuestionIndex = 0;
+
+function displayQuestion(){
+    if (currentQuestionIndex < questions.length){
+        questionElement.textContent = questions[currentQuestionIndex].question;
+        userGuess.value = '';
+        outputElement.textContent = '';
+    }
+    else {
+        questionElement.textContent = "Congratulation! You completed all questions!";
+        userGuess.style.display = 'none'
+        submitButton.style.display = 'none'
+
+    }
+}
+    
 function checkanswer() {
     const value = userGuess.value;  // Get the actual value (string)
     const numValue = parseInt(value);  // Convert to number if needed
+    const correctAnswer = questions[currentQuestionIndex].answer;
     
-    if (numValue === 3) {
-        alert('Hello World');
+    if (numValue === correctAnswer) {
+        outputElement.textContent = "correct"
+        setTimeout(() => {
+            currentQuestionIndex++;
+            displayQuestion()
+        }, 1000);
+    }else {
+            outputElement.textContent = `Incorrect, The answer is ${correctAnswer}`; 
+        }
     }
-    else{
-        
-    }
-}
 
-document.getElementById('submitButton').addEventListener('click', checkanswer)
+
+submitButton.addEventListener('click', checkanswer);
+
+displayQuestion();
